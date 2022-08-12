008c29ae8348ce3df30339950705c0642dcd0fee
MaxX <maxx-f@mail.ru>
2022-08-12 16:05:31 +0300

    Add DZ_003

---
+++ DZ_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+void NumberN(int number)
+{
+    int count = 1;
+    while(count < number)
+    {
+        Console.Write($"{count*count*count}, ");
+        count++;
+    }
+    Console.Write($"{count*count*count}. ");
+}
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+NumberN(num);
